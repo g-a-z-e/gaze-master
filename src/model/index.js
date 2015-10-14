ab8b8bc135d3876b1d9bf33379793e507360a360
Rube <353371737@qq.com>
2015-10-14 23:05:09 +0800

    model index change

--- src/model/index.js
+++ src/model/index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const config = require('../config');
 const User = require('./user');
 const Admin = require('./admin');
+const Group = require('./group').Group;
 
 mongoose.connect(config.db.url, err=> {
     if (err) {
@@ -11,3 +12,4 @@ mongoose.connect(config.db.url, err=> {
 
 exports.User = mongoose.model('User');
 exports.Admin = mongoose.model('Admin');
+exports.Group = mongoose.model('Group');
