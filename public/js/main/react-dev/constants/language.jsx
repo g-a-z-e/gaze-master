922bd9209d6d7e48a456dc133e815f551bdc256c
Rube <353371737@qq.com>
2015-10-23 01:23:28 +0800

    全局语言

---
+++ public/js/main/react-dev/constants/language.jsx
@@ -0,0 +1,11 @@
+/**
+ * @module
+ * @author Rube
+ * @date 15/10/26
+ * @desc
+ */
+/** group **/
+export const G_NO_SELECTED = 'no selected';
+export const G_CREATE_SUCCESS = 'Success';
+export const G_CREATE_ERROR = 'Error';
+export const G_CREATE = 'Create';
