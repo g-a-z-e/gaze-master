7f2e574cfaf86e5bac2d5af5c05965f8e2867da0
Rube <353371737@qq.com>
2015-10-27 02:48:51 +0800

    add react-d3 (have change)

---
+++ public/js/main/react-dev/charts/common/index.jsx
@@ -0,0 +1,7 @@
+
+exports.XAxis = require('./axes/index').XAxis;
+exports.YAxis = require('./axes/index').YAxis;
+exports.Chart = require('./charts/index').Chart;
+exports.LegendChart = require('./charts/index').LegendChart;
+exports.Legend = require('./Legend');
+exports.Voronoi = require('./Voronoi');
---
+++ public/js/main/react-dev/charts/index.jsx
@@ -0,0 +1,8 @@
+exports.BarChart = require('./barchart/index').BarChart;
+exports.LineChart = require('./linechart/index').LineChart;
+exports.PieChart = require('./piechart/index').PieChart;
+exports.AreaChart = require('./areachart/index').AreaChart;
+exports.Treemap = require('./treemap/index').Treemap;
+exports.ScatterChart = require('./scatterchart/index').ScatterChart;
+exports.CandlestickChart = require('./candlestick/index').CandlestickChart;
+
